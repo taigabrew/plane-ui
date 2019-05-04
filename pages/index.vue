753ebe2ac9634b0fae17c483d88ae37b50496588
Dan Kazakov <taigabrew@gmail.com>
2019-05-04 11:21:27 +0300

    сообщение "wip" на главной

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
   <section>
-    <div class="mb-6 flex justify-between">
+    <!-- <div class="mb-6 flex justify-between">
       <h1 class="font-bold text-sm tracking-wide uppercase text-gray-600">
         Привет
       </h1>
       <div>контекстные действия</div>
-    </div>
-    <div class="bg-white rounded shadow-md p-16">
-      piu-piu
+    </div> -->
+    <div
+      class="bg-white rounded shadow-md py-6 px-8 border-l-8 border-red-500 text-red-800"
+    >
+      Проект находиться в разработке
     </div>
   </section>
 </template>

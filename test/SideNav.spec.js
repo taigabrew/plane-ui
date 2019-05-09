1e2cde1c603083b504105f4f65864b49efc87ee7
Dan Kazakov <taigabrew@gmail.com>
2019-05-09 21:05:53 +0300

    test change

--- test/SideNav.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import SideNav from '@/components/SideNav.vue'
-
-describe('SideNav', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(SideNav)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
